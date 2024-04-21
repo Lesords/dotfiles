3925a5d91361ad2ee3b2d1990557f7b2fd1d461d
Lesords <2385342343@qq.com>
2024-04-21 22:40:22 +0800

    chore: add some commonly used key mappings

--- .config/lese/keymap.sh
+++ .config/lese/keymap.sh
@@ -4,3 +4,9 @@ if type tree >/dev/null 2>&1; then
     bind '";t": "\C-utree .\C-m"'
 fi
 bind '";u": "\C-udu -sh *\C-m"'
+bind '";l": "\C-ulsblk\C-m"'
+bind '";s": "\C-uls `la`\C-m"'
+bind '";mt": "\C-usudo mount"'
+bind '";mr": "\C-usudo umount"'
+bind '";f": "\C-ufind . -type f -name \""'
+bind '";g": "\C-ugit clone --depth 1 --recurse-submodules --shallow-submodules "'
