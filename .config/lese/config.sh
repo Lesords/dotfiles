11cc589eebdb7caef721560a76d96b1a5f8c507d
Lesords <2385342343@qq.com>
2024-05-06 01:45:56 +0000

    fix: solved the problem of get-gpg-id alias command

--- .config/lese/config.sh
+++ .config/lese/config.sh
@@ -15,7 +15,7 @@ alias diff-better='diff-so-fancy | slow'
 alias gen-key='ssh-keygen -m PEM -t rsa -b 4096 -C' # with comment
 alias test-github='ssh -T git@github.com'
 alias gen-gpg='gpg --full-generate-key'
-alias get-gpg-id='gpg --list-secret-keys --keyid-format=long | grep "sec" | cut -d "/" -f 2 | cut -d " " -f 1'
+alias get-gpg-id='gpg --list-secret-keys --keyid-format=long 2> /dev/null | grep "sec" | cut -d "/" -f 2 | cut -d " " -f 1'
 alias get-gpg-key='gpg --armor --export `get-gpg-id`'
 
 alias get-my-tool='git clone git@github.com:Lesords/My-Linux-Tool.git --depth=1'
