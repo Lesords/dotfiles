973ad58f61d49c641c10372b1a035dd9c470dabf
Lesords <2385342343@qq.com>
2025-02-11 06:18:13 +0000

    feat: add a shortcut key to open the file manager in WSL and git bash environment

--- .config/lese/keymap.sh
+++ .config/lese/keymap.sh
@@ -4,6 +4,9 @@ if [ -t 1 ]; then
     if type tree >/dev/null 2>&1; then
         bind '";t": "\C-utree .\C-m"'
     fi
+    if [[ "`uname -r | grep WSL`" || "$MSYSTEM" ]]; then
+        bind '";o": "\C-uexplorer.exe .\C-m"'
+    fi
     bind '";n": "\C-uclear -x\C-m"'
     bind '";u": "\C-udu -sh *\C-m"'
     bind '";l": "\C-ulsblk\C-m"'
