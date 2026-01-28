864a1fb25f853c059387d4db6570ee779057ae6d
Lesords <lesords168@gmail.com>
2026-01-29 00:08:03 +0800

    perf: optimize the display effect of the render-markdown plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -38,6 +38,8 @@ require("CopilotChat").setup({
     auto_insert_mode = true,     -- Enter insert mode when opening
 })
 
+vim.api.nvim_set_hl(0, "RenderMarkdownInlineHighlight", { fg = "#E39AA6", bg = "#1a190c", bold = true })
 require('render-markdown').setup({
     file_types = { 'markdown', 'codecompanion', 'copilot-chat' },
+    render_modes = { "n", "no", "c", "t", "i", "ic" },
 })
