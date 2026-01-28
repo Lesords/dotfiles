43c1cea1c4632a5989bb1b4ae4c4cabcbd4cdba4
Lesords <lesords168@gmail.com>
2026-01-28 20:13:22 +0800

    feat: add configuration related to copilotchat in nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,43 @@
+vim.opt.splitright = true
+
 vim.cmd [[
     set runtimepath^=~/.vim runtimepath+=~/.vim/after
     let &packpath = &runtimepath
     source ~/.vim/vimrc
 ]]
+
+-- CopilotChat
+vim.api.nvim_set_hl(0, 'CopilotChatHeader', { fg = '#7C3AED', bold = true })
+vim.api.nvim_set_hl(0, 'CopilotChatSeparator', { fg = '#374151' })
+
+require("CopilotChat").setup({
+    model = 'claude-sonnet-4.5',
+    -- model = "gemini-3-pro-preview",
+
+    highlight_headers = true,
+    error_header = '> [!ERROR] Error',
+    temperature = 0.1,           -- Lower = focused, higher = creative
+
+    headers = {
+        user = '👤 You',
+        assistant = '🤖 Copilot',
+        tool = '🔧 Tool',
+    },
+
+    window = {
+        layout = 'vertical',
+        width = 0.4, -- Fixed width in columns
+        height = 20, -- Fixed height in rows
+        border = 'rounded', -- 'single', 'double', 'rounded', 'solid'
+        title = '🤖 AI Assistant',
+        zindex = 100, -- Ensure window stays on top
+    },
+
+    separator = '━━',
+    auto_fold = true, -- Automatically folds non-assistant messages
+    auto_insert_mode = true,     -- Enter insert mode when opening
+})
+
+require('render-markdown').setup({
+    file_types = { 'markdown', 'codecompanion', 'copilot-chat' },
+})
