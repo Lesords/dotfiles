2fe84a45a1c94cf34ab4e0a807f33db25384fcf6
Lesords <lesords168@gmail.com>
2026-01-30 20:03:50 +0800

    feat: add relevant configuration content for the blink.cmp plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -46,3 +46,60 @@ require('render-markdown').setup({
     file_types = { 'markdown', 'codecompanion', 'copilot-chat' },
     render_modes = { "n", "no", "c", "t", "i", "ic" },
 })
+
+-- Blink Cmp
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#282828", fg= "#928374" })
+vim.api.nvim_set_hl(0, "Pmenu", { bg = "#282828", fg= "#928374" })
+
+require('blink.cmp').setup({
+    keymap = {
+        preset = "default",
+        ["<S-Tab>"] = { "select_prev", "fallback" },
+        ["<Tab>"] = { "select_next", "fallback" },
+    },
+    appearance = {
+        nerd_font_variant = 'mono'
+    },
+    completion = {
+        ghost_text = { enabled = true },
+        menu = {
+            border = "rounded",
+        },
+        documentation = {
+            auto_show = true,
+            auto_show_delay_ms = 500,
+            treesitter_highlighting = true,
+            window = {
+                border = 'rounded',
+            }
+        }
+    },
+    sources = {
+        per_filetype = {
+            -- Add the copilot-chat source for relevant filetype
+            ["copilot-chat"] = { "copilotchat" },
+        },
+        default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
+        providers = {
+            path = {
+                -- Path sources triggered by "/" interfere with CopilotChat commands
+                enabled = function()
+                    return vim.bo.filetype ~= "copilot-chat"
+                end,
+            },
+            copilotchat = {
+                name = "CopilotChat",
+                module = "blink-cmp-copilot-chat",
+            },
+            copilot = {
+                name = "copilot",
+                module = "blink-copilot",
+                score_offset = 100,
+                async = true,
+            },
+        },
+    },
+    fuzzy = {
+        implementation = "lua",
+    }
+})
