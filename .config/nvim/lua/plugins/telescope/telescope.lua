ce177ef77703c4704e3dd68fc2c964b8280fd00a
Lesords <lesords168@gmail.com>
2026-03-06 00:31:54 +0800

    feat: add some keyboard shortcuts for telescope plugin in Neovim

--- .config/nvim/lua/plugins/telescope/telescope.lua
+++ .config/nvim/lua/plugins/telescope/telescope.lua
@@ -4,5 +4,7 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = "Telescope search current word" })
+vim.keymap.set('n', '<leader>fl', builtin.current_buffer_fuzzy_find, { desc = "Telecope search curent buffer" })
 
 require('telescope').setup({})
